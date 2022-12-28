d2afff996380888ec59e972cba633313e21484e1
brianley99 <Brianley99@gmail.com>
2022-12-28 03:57:55 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContactPro_V2._3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
